19be103252c732e870050a3d5ab3f8ccfaeb92c5
Ashish Verma <vermaashish322@gmail.com>
2024-01-20 04:41:43 +0530

    create signup route

--- mern-auth/api/index.js
+++ mern-auth/api/index.js
@@ -1,8 +1,9 @@
 // const express = require("express");
-import express from 'express';
+import express, { urlencoded } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -12,9 +13,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express();
+app.use(urlencoded({extended : true}));
+app.use(express.json());
 
 app.listen(3000, ()=>{
     console.log("server listening on port 3000");
 })
 
 app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes);
