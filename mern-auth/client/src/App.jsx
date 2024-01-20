fc5a0f6bcfd88c66df33125ce0a4dbcb88807c09
Ashish Verma <vermaashish322@gmail.com>
2024-01-20 23:36:09 +0530

    update header component and made profile private

--- mern-auth/client/src/App.jsx
+++ mern-auth/client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 import Header from './components/Header';
 import SignUp from './pages/SignUp';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return ( 
@@ -13,9 +14,11 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/about' element={<About/>}></Route>
-      <Route path='/profile' element={<Profile/>}></Route>
       <Route path='/sign-in' element={<SignIn />}></Route>
       <Route path='/sign-up' element={<SignUp/>}></Route>
+      <Route element={<PrivateRoute/>}>
+        <Route path='/profile' element={<Profile />} />
+      </Route>
     </Routes>
     </BrowserRouter>
   );
