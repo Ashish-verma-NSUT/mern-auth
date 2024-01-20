4a7ef30e44792af4e271ea85fc1b10effe47cd55
Ashish Verma <vermaashish322@gmail.com>
2024-01-20 15:24:08 +0530

    complete signup page ui

--- mern-auth/client/src/App.jsx
+++ mern-auth/client/src/App.jsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
-import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 import Header from './components/Header';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   return ( 
-  <BrowserRouter>
-  <Header />
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/about' element={<About />} />
-    <Route path='/sign-in' element={<SignIn />} />
-    <Route path='/sign-up' element={<SignUp />} />
-    <Route path='/profile' element={<Profile />} />
-  </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+      <Route path='/sign-in' element={<SignIn />}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
     
 }
--- mern-auth/client/src/pages/About.jsx
+++ mern-auth/client/src/pages/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function About() {
-  return (
-    <h1>About</h1>
-  )
+  return  <h1>About</h1>
+  
 }
