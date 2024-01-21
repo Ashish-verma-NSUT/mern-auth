7c7c82b545351719d21d5b98616d1b6f5c8193f6
Ashish Verma <vermaashish322@gmail.com>
2024-01-22 04:06:15 +0530

    add pre production code

--- mern-auth/api/index.js
+++ mern-auth/api/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -15,6 +16,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express();
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 app.use(urlencoded({extended : true}));
 app.use(express.json());
 app.use(cookieParser());
