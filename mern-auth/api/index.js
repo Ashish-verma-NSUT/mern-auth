d8b92ca0222f7c02ac4ba044731959c20d688052
Ashish Verma <vermaashish322@gmail.com>
2024-01-22 02:54:40 +0530

    complete update user functionality

--- mern-auth/api/index.js
+++ mern-auth/api/index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,6 +17,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app = express();
 app.use(urlencoded({extended : true}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log("server listening on port 3000");
